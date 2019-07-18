5ab3a760d10084233bdd12b7c7ba0fab0e4c9d77
Ziwei <jiangziwei.j@gmail.com>
2019-07-18 12:39:33 -0700

    fixed the bug in react component

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 //we have to import React as we are writing JSX below, not HTML....
 
 
-const Card =({name, email, id})=>{
+class Card extends Component {
+  render(){
   return(
      <div className = 'bg-light-green dib br3 pa3 ma2 grow shadow-5'>
       <div>
         <h1>can only return 1 thing</h1>
       </div>
-      <img alt = 'robots' src={'https://robohash.org/${id}?200x200'} />
+      <img alt = 'robots' src={'https://robohash.org/${this.props.id}?200x200'} />
       <div>
-        <h2>{id}</h2>
-        <p>{name}</p>
+        <h2>{this.props.id}</h2>
+        <p>{this.props.name}</p>
       </div>
     </div>
 
   )
+}
 }
 //All JS expressions have to wrapped in {}, this is a JSX thing
 export default Card;
